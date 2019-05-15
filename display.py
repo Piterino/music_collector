9de1f9c9adcabe109ba3af32529c5b61e6051568
Peter <peter.lyskawa1995@gmail.com>
2019-05-15 14:33:24 +0200

    Music collector teamwork
    Repo setup

---
+++ display.py
@@ -0,0 +1,6 @@
+from music_reports import sort_by_user_arg, convert_txt_to_data
+#Functions, return values (eg sorted data)
+
+def show_user_choice(sort_by = None):
+    pass
+
---
+++ music_reports.py
@@ -0,0 +1,15 @@
+def sort_by_user_arg(user_argument = 'sort by ex. artist'):
+    """
+    Sort what the user wants to see by argument with a loop
+    """
+    pass
+
+def convert_txt_to_data(file_to_read = 'text_albums_data.txt'):
+    """
+    Read the txt file and put it into a decque/list/tuple/dictionary/whatever
+    in the given order: artist name,album name,release year,genre,length.
+    """
+    print(file_to_read)
+    pass
+
+convert_txt_to_data('text_albums_data.txt')
