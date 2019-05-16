2c07a72a99f3736fce662041bf4c46b3bf33e6a6
Peter <peter.lyskawa1995@gmail.com>
2019-05-16 14:09:57 +0200

    16.05 14:10 Wersja pseudo dziala

--- music_reports.py
+++ music_reports.py
@@ -1,15 +1,42 @@
+def read_file(file_to_read = 'text_albums_data.txt'):
+    """
+    Read the txt file
+    """
+    with open(file_to_read) as f:
+        music_data = f.readlines()
+    for index, item in enumerate(music_data):
+        music_data[index] = item.strip()
+    for index, item in enumerate(music_data):
+        music_data[index] = item.split('\n')
+    return music_data
+
+def convert_txt_to_data():
+    """
+    Put it into a decque/list/tuple/dictionary/whatever
+    in the given order: artist,album, year,genre,length.
+    """
+    pass
+
+def sort_music_data(sort_by = None):
+    """
+    Take the music_data file from convert_txt_to_data and sort it by user choice
+    Put it into a decque/list/tuple/dictionary/whatever
+    in the given order: artist,album, year,genre,length.
+    """
+    for lists in read_file():
+        print(lists)
+    pass
+
 def sort_by_user_arg(user_argument = 'sort by ex. artist'):
     """
     Sort what the user wants to see by argument with a loop
     """
     pass
 
-def convert_txt_to_data(file_to_read = 'text_albums_data.txt'):
+def data_music_parcelled():
     """
-    Read the txt file and put it into a decque/list/tuple/dictionary/whatever
-    in the given order: artist name,album name,release year,genre,length.
+    Segregate the data into small chunks for easy access by display.py
     """
-    print(file_to_read)
     pass
 
-convert_txt_to_data('text_albums_data.txt')
+sort_music_data()
